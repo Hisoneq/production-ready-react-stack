4f4ad1793b6ad8d6ba4a944139f9c8f1e46e7017
Raman <watuwnnt@gmail.com>
2025-12-16 18:46:02 +0300

    feat: react and css/scss loaders

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './Counter.scss';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p>{count}</p>
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
+}
