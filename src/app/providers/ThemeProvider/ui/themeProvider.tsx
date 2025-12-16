1880de81988f83337d8971c366b69c9094a77982
Raman <watuwnnt@gmail.com>
2025-12-17 02:00:19 +0300

    feat: FSD architecture

--- src/app/providers/ThemeProvider/ui/themeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/themeProvider.tsx
@@ -1,6 +1,6 @@
-import { LOCAL_STORAGE_THEME_KEY, ThemeContext } from "./themeContext";
+import { LOCAL_STORAGE_THEME_KEY, ThemeContext } from "../lib/themeContext";
 import { useMemo, useState } from "react";
-import { Theme } from "./themeContext";
+import { Theme } from "../lib/themeContext";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.DARK;
 
