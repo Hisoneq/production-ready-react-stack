de3fb68c6e9d9b0f9adefd174e5f629cfa9c0322
Raman <watuwnnt@gmail.com>
2025-12-29 23:23:04 +0300

    feat: login form and login modal

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,7 @@
 import { AppLink } from './AppLink/AppLink';
 import { Button, ButtonTheme } from './Button/Button';
+import { Input } from './Input/Input';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 
-export { AppLink, Button, ButtonTheme, Loader, Modal };
+export { AppLink, Button, ButtonTheme, Input, Loader, Modal };
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,7 +1,8 @@
+import { LoginModal } from 'features/AuthByUsername';
 import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
-import { Button, ButtonTheme, Modal } from 'shared/ui';
+import { Button, ButtonTheme } from 'shared/ui';
 import cls from './NavBar.module.scss';
 
 interface NavbarProps {
@@ -12,18 +13,20 @@ export function NavBar({ className }: NavbarProps) {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <Button theme={ButtonTheme.OUTLINE} className={cls.links} onClick={onToggleModal}>
+            <Button theme={ButtonTheme.OUTLINE} className={cls.links} onClick={onShowModal}>
                 {t('Войти')}
             </Button>
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                {t('Пример текста модалки')}
-            </Modal>
+            <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
         </div>
     );
 }
